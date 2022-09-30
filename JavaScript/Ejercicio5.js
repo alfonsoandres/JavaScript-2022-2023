e23ca6f310e06f5ea7c44ca12afecdcb2ae8be01
alfonsoandres <114391715+alfonsoandres@users.noreply.github.com>
2022-09-30 11:23:43 +0200

    Add files via upload
    commit -m

---
+++ JavaScript/Ejercicio5.js
@@ -0,0 +1,23 @@
+    var lineas=prompt("dime cuntas lineas quieres");
+
+for (var i = 1; i <= lineas; i++)
+    {
+        var k;
+        for (k = i; k < lineas; j++)
+        {
+        
+            document.write(" ");
+        }
+    }
+
+
+for (var j = 1; i < i*2-1; i++)
+    {
+        
+        document.write("*");
+    }
+
+
+
+
+
